1a9e0ec6b8ad2c3e55b42437601eaf6480b0272e
sslinNn <morison1991@mail.ru>
2026-07-17 10:00:35 +0700

    Scaffold devalerts package with uv

---
+++ src/devalerts/__init__.py
@@ -0,0 +1,5 @@
+"""Throwaway prototype: send unhandled Python exceptions straight to a Telegram chat."""
+
+from __future__ import annotations
+
+__all__: list[str] = []
---
+++ tests/test_devalerts.py
@@ -0,0 +1,5 @@
+import devalerts
+
+
+def test_package_imports():
+    assert devalerts is not None
